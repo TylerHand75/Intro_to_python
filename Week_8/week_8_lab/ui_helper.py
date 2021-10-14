4cb1165ae66514719b1444284ff8013e8b2de8e5
Tyler Hand <tylerhand75@gmail.com>
2021-10-14 10:51:41 -0500

    week 8 lab

---
+++ Week_8/week_8_lab/ui_helper.py
@@ -0,0 +1,141 @@
+#   FILE:   ui_helper.py
+#   DATE:   2021-10-07
+#   AUTHOR: Tyler Hand
+#   DESCRIPTION:
+"""
+helpful functions for the user interface. 
+
+user interface is part of the code that interacts with the user.
+
+MOD: 2021-10-14
+MOD: tyler hand 
+MOD discription: added function to get_user_int(). added a get_user_float.
+added console width 
+
+"""
+
+
+CONSOLE_WIDTH = 80
+"""
+default console width 
+"""
+# #############################################################################
+# Function for general user interaction
+# these functions separate the view logic from the control and model logic.
+# #############################################################################
+
+
+def show_program_title(title, WIDTH):
+    """Displays the program title in a consitent manor 
+
+    inputs: title sting with the title of the program 
+    width the width of the screen, used for centering.
+    """
+    new_title = '*** ' + title.upper() + ' ***'
+    print(f'\n{new_title:^{WIDTH}}')
+
+
+def show_section_title(title):
+    """
+        displays tehh section title in a consistant format
+    inputs:
+        titel string conatining the title text of the section
+    """
+    print(f'\n\t-- {title}--')
+
+
+def press_enter_to_continue():
+    """
+        display a message telling the user to press the Enter key to continue
+        Wait untill they do. ignore anything else they enter.    
+    """
+    input('\nPress the Enter key to continue...')
+
+def show_message(message):
+    """
+    displays a message in a consistant format.
+    input: message a string 
+    """
+    print(message)
+
+def get_user_int(prompt):
+    """
+    prompt the user to enter the int and return that int value. 
+
+    Inputs:
+        prompt string a string with instructions for the user     
+    Returns:
+        int the int value entered by the user. 
+    """
+    return int(input(prompt + " "))
+
+def get_user_float(prompt):
+    """
+    prompt the user to enter the float and return that float value. 
+
+    Inputs:
+        prompt string a string with instructions for the user     
+    Returns:
+        float the float value entered by the user.
+    """
+    return float(input(prompt + " "))
+
+def show_bar():
+    """
+    draw a horizontal line made of '*' chartictars all the way across.
+    """
+    bar = '*' * CONSOLE_WIDTH
+    show_message(bar)
+
+
+
+
+
+
+# #############################################################################
+# menu functions
+# #############################################################################
+
+def get_user_choice():
+    """
+        display the menu and ask the user to do something 
+       Returns:
+            string user's choice
+    """
+    menu_title = 'Main Menu'
+    prompt = "Your choice: "
+    options = ['1) Add an employee ', '2) show all employee ',
+               '3) view employee records ', '4) update an employee record ',
+               '5) delete an employee record ', '6) Quit ']
+    show_menu(menu_title, options)
+    return get_user_string(prompt)
+
+
+def show_menu(menu_title, options):
+    """
+        display a menu un a stuctured format
+        input: 
+        menu_title a string with the title of a menu 
+        Optionsa list containing strings for the menu
+    """
+    print(f'\n\n\t--{menu_title}--')
+    for option in options:
+        print(f'\t {option}')
+
+
+def get_user_string(prompt):
+    """
+        prompts the user to enter a string and returns the string
+    """
+    return input(prompt + " ")
+
+
+def confirm_quit():
+    """
+        displpay a message stating the user has chosen to Quit
+        ask the user to confirm by entering Y or N. if the user enters
+        Y or y the return True
+    """
+    print('You Chose to Quit')
+    choice = get_user_string('Confirm (Y/N)')
+    return choice == 'Y' or choice == 'y'
